e43635b42955ab453b8b539b34a64038bfecfc96
synbear <ahmedbukhari.uni@gmail.com>
2025-11-10 20:50:08 +0500

    auth url fix

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(!auth.isAuthenticated) navigate('/auth/next=/');
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
   }, [auth.isAuthenticated])
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
